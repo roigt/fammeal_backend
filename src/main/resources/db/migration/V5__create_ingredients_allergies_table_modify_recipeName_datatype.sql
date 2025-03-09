5e97c84a59dfb8b8db07ec0490a997785cc8361c
reda <reda.himmi@edu.uca.ma>
2025-03-09 15:51:59 +0100

    fix: add flyway migration files

---
+++ src/main/resources/db/migration/V5__create_ingredients_allergies_table_modify_recipeName_datatype.sql
@@ -0,0 +1,15 @@
+CREATE TABLE ingredients_allergies
+(
+    allergy_id    BIGINT NOT NULL,
+    ingredient_id UUID   NOT NULL,
+    CONSTRAINT pk_ingredients_allergies PRIMARY KEY (allergy_id, ingredient_id)
+);
+
+ALTER TABLE ingredients_allergies
+    ADD CONSTRAINT fk_ingall_on_allergy_entity FOREIGN KEY (allergy_id) REFERENCES allergies (id);
+
+ALTER TABLE ingredients_allergies
+    ADD CONSTRAINT fk_ingall_on_ingredient_entity FOREIGN KEY (ingredient_id) REFERENCES ingredients (idIngredient);
+
+ALTER TABLE recipes
+    ALTER COLUMN recipeName TYPE VARCHAR(30) USING (recipeName::VARCHAR(30));
